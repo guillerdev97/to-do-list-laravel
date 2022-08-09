0efa9f15dd5ac139f7214f5a6bf21c36758f844b
Guillermo García <guillermieres97@gmail.com>
2022-08-09 11:40:25 +0200

    changes on styles

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,6 +1,13 @@
 @extends('app')
 
 @section('content')
-    <p>Hello World</p>
+<div class="container w-25 border mt-4 p-4">
+    <form>
+        <div class="mb-3">
+            <label for="taskTitle" class="form-label">Task title:</label>
+            <input type="text" id="taskTitle" name="taskTitle" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Create new task</button>
+    </form>
+</div>
 @endsection
-
